de91795331b6953b20dae30003795fef3f8146fc
Алёна <monarxxii@gmail.com>
2025-12-16 21:05:55 +0700

    отредактировала первую сцену

--- Assets/Scripts/Windows/BootMenu.cs
+++ Assets/Scripts/Windows/BootMenu.cs
@@ -3,9 +3,49 @@ using UnityEngine.SceneManagement;
 
 public class BootMenu : MonoBehaviour
 {
+    [Header("UI Roots")]
+    [SerializeField] private GameObject mainCard;
+    [SerializeField] private GameObject controlsPanel;
+    [SerializeField] private GameObject hintsPanel;
+
+    private void Awake()
+    {
+        ShowMain();
+    }
+
     public void StartGame()
     {
         Debug.Log("START CLICKED");
         SceneManager.LoadScene(1); // 01_Game
+    }
+
+    public void OpenControls()
+    {
+        HideAll();
+        if (controlsPanel != null) controlsPanel.SetActive(true);
+    }
+
+    public void OpenHints()
+    {
+        HideAll();
+        if (hintsPanel != null) hintsPanel.SetActive(true);
+    }
+
+    public void BackToMain()
+    {
+        ShowMain();
+    }
+
+    private void ShowMain()
+    {
+        HideAll();
+        if (mainCard != null) mainCard.SetActive(true);
+    }
+
+    private void HideAll()
+    {
+        if (mainCard != null) mainCard.SetActive(false);
+        if (controlsPanel != null) controlsPanel.SetActive(false);
+        if (hintsPanel != null) hintsPanel.SetActive(false);
     }
 }
